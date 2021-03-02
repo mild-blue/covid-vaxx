58d70b15a98679a07d2b7a0c65e589e4157ac62b
Tomas Pavlin <tomas.pavlin@gmail.com>
2021-03-02 14:52:17 +0100

    Introduce PatientInfo

---
+++ frontend/src/app/model/PatientInfo.ts
@@ -0,0 +1,17 @@
+export class PatientInfo {
+  first_name: string;
+  last_ame: string;
+  birth_number: number;
+  insurance_company: string;
+  phone_number: string;
+  email: string;
+
+  is_sick: boolean;
+  did_have_covid: boolean;
+  is_vaccinated_against_covid: boolean;
+  had_allergic_reaction: boolean;
+  has_blood_problems: boolean;
+  has_immunity_problems: boolean;
+  is_pregnant: boolean;
+  is_vaccinated_in_last_two_weeks: boolean;
+}
