71c1a316a6c712e00c76aa9c4b38d392ce3f566b
Lukas Forst <lukas@forst.pw>
2021-03-02 22:46:22 +0100

    fix naming

--- backend/src/main/kotlin/blue/mild/covid/vaxx/routes/Routes.kt
+++ backend/src/main/kotlin/blue/mild/covid/vaxx/routes/Routes.kt
@@ -1,12 +1,12 @@
 package blue.mild.covid.vaxx.routes
 
 object Routes {
-    val patient = apiName("/patient")
-    val question = apiName("/question")
+    val patient = apiName("patient")
+    val question = apiName("question")
 
-    val version = apiName("/version")
-    val status = apiName("/status")
-    val statusHealth = apiName("/status/health")
+    val version = apiName("version")
+    val status = apiName("status")
+    val statusHealth = apiName("status/health")
 
     private fun apiName(name: String) = "/api/$name"
 }
