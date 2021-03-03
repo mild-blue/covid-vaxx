62f32e8e075c2f3a93cfefd2429592e27e63f569
anastasia <anastasia.surikova@akka.eu>
2021-03-03 21:22:08 +0100

    load questions from be

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { PatientInfo } from './model/PatientInfo';
+import { PatientInfo, YesNoQuestion } from './model/PatientInfo';
 import { InsuranceCompany } from './model/InsuranceCompany';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from './components/dialog/dialog.component';
 import { PatientService } from './services/patient/patient.service';
 import { validatePersonalNumber, validatePhoneNumber } from './app.validators';
+import { QuestionService } from '@app/services/question/question.service';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,7 @@ export class AppComponent implements OnInit {
   public basicInfoForm?: FormGroup;
 
   public patientInfo: PatientInfo = new PatientInfo();
+  public questions: YesNoQuestion[] = [];
   public allInsuranceCompanies: string[] = Object.values(InsuranceCompany);
 
   public agreementCheckboxValue: boolean = false;
@@ -24,6 +26,7 @@ export class AppComponent implements OnInit {
 
   constructor(private _formBuilder: FormBuilder,
               private _dialog: MatDialog,
+              private _questionService: QuestionService,
               private _patientService: PatientService) {
   }
 
@@ -36,10 +39,16 @@ export class AppComponent implements OnInit {
       phone: ['', [Validators.required, validatePhoneNumber]],
       email: ['', [Validators.required, Validators.email]]
     });
+
+    this._initQuestions();
+  }
+
+  private async _initQuestions(): Promise<void> {
+    this.questions = await this._questionService.getQuestions();
   }
 
   get allQuestionsAnswered(): boolean {
-    const unanswered = this.patientInfo.questions.filter(q => q.value === undefined);
+    const unanswered = this.questions.filter(q => q.value === undefined);
     return unanswered.length === 0;
   }
 
--- frontend/src/app/services/question/question.service.ts
+++ frontend/src/app/services/question/question.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Question } from '@app/model/Question';
+import { environment } from '@environments/environment';
+import { first, map } from 'rxjs/operators';
+import { parseQuestion } from '@app/parsers/question.parser';
+import { YesNoQuestion } from '@app/model/PatientInfo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) {
+  }
+
+  public async getQuestions(): Promise<YesNoQuestion[]> {
+    return this._http.get<Question[]>(
+      `${environment.apiUrl}/question`
+    ).pipe(
+      first(),
+      map(response => response.map(parseQuestion))
+    ).toPromise();
+  }
 }
