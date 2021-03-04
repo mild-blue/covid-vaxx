a82671db5b20d1a36a811de05a38a041e4363800
Tomáš Kouřim <kourim@outlook.com>
2021-03-04 23:21:36 +0100

    next db questions typo

--- backend/src/main/resources/db/migration/V3__typo.sql
+++ backend/src/main/resources/db/migration/V3__typo.sql
@@ -1,4 +1,8 @@
 -- update question data
 UPDATE questions
+SET cs='Cítíte se v tuto chvílí nemocný/á?'
+WHERE id = '9a5587a1-dc43-49f3-9847-b736127c9e39';
+
+UPDATE questions
 SET cs='Absolvoval/a jste v posledních dvou týdnech nějaké jiné očkování?'
-WHERE id = 'f4ca8d25-faaa-4b2f-abc2-3d7a8702d4a3'
+WHERE id = 'f4ca8d25-faaa-4b2f-abc2-3d7a8702d4a3';
