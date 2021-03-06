19755d93cd32ff8a2704241b6a9fcedb5b916c4d
Do <thang.do@firma.seznam.cz>
2021-03-06 15:18:20 +0100

    Setting up admin page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '@app/pages/home/home.component';
+import { AdminComponent } from '@app/pages/admin/admin.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent }, // fully match an empty route
+  { path: 'admin', pathMatch: 'full', component: AdminComponent }, 
 
   // Redirect all to HomeComponent
   { path: '**', redirectTo: '' }
