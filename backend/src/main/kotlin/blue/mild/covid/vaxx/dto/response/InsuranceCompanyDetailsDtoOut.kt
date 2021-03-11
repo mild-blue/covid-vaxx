5cefa3199df8b14cbaff50bdda0617f2615e3b2c
Lukas Forst <lukas@forst.pw>
2021-03-11 10:31:49 +0100

    make insurance company dto serializable dto

--- backend/src/main/kotlin/blue/mild/covid/vaxx/dto/response/InsuranceCompanyDetailsDtoOut.kt
+++ backend/src/main/kotlin/blue/mild/covid/vaxx/dto/response/InsuranceCompanyDetailsDtoOut.kt
@@ -1,13 +1,13 @@
 package blue.mild.covid.vaxx.dto.response
 
 import blue.mild.covid.vaxx.dao.model.InsuranceCompany
-import com.fasterxml.jackson.annotation.JsonIgnore
 
 data class InsuranceCompanyDetailsDtoOut(
-    @JsonIgnore
-    private val insuranceCompany: InsuranceCompany
+    val name: String,
+    val csFullName: String,
+    val code: InsuranceCompany
 ) {
-    val name = insuranceCompany.name
-    val csFullName = insuranceCompany.csFullName
-    val code = insuranceCompany.code
+    constructor(company: InsuranceCompany) : this(
+        name = company.name, csFullName = company.csFullName, code = company
+    )
 }
