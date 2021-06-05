8651b3988ca229e8e389875d8e3629d7263e76c8
Lukas Forst <lukas@forst.pw>
2021-06-05 14:47:01 +0200

    add more test cases for patient registration

--- backend/src/test/kotlin/blue/mild/covid/vaxx/routes/PatientRoutesTest.kt
+++ backend/src/test/kotlin/blue/mild/covid/vaxx/routes/PatientRoutesTest.kt
@@ -10,7 +10,9 @@ import blue.mild.covid.vaxx.dto.request.AnswerDtoIn
 import blue.mild.covid.vaxx.dto.request.ConfirmationDtoIn
 import blue.mild.covid.vaxx.dto.request.CreateVaccinationSlotsDtoIn
 import blue.mild.covid.vaxx.dto.request.LocationDtoIn
+import blue.mild.covid.vaxx.dto.request.PatientUpdateDtoIn
 import blue.mild.covid.vaxx.dto.request.PhoneNumberDtoIn
+import blue.mild.covid.vaxx.dto.response.PatientDtoOut
 import blue.mild.covid.vaxx.dto.response.PatientRegistrationResponseDtoOut
 import blue.mild.covid.vaxx.generators.generatePatientRegistrationDto
 import blue.mild.covid.vaxx.service.LocationService
@@ -34,8 +36,10 @@ import org.kodein.di.bind
 import org.kodein.di.instance
 import org.kodein.di.singleton
 import java.time.Instant
+import java.util.UUID
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class PatientRoutesTest : ServerTestBase() {
 
@@ -66,6 +70,97 @@ class PatientRoutesTest : ServerTestBase() {
             service
         }
     }
+
+    @Test
+    fun `complete crud on patient`() = withTestApplication {
+        val createSlots = CreateVaccinationSlotsDtoIn(
+            locationId = locationId,
+            from = Instant.ofEpochMilli(20),
+            to = Instant.ofEpochMilli(30),
+            durationMillis = 10,
+            bandwidth = 1,
+        )
+        val slotService by closestDI().instance<VaccinationSlotService>()
+        val slots = runBlocking { slotService.addSlots(createSlots) }
+        assertEquals(1, slots.size)
+
+        val validRegistration = runBlocking { generatePatientRegistrationDto() }
+        // create patient
+        val patientId = handleRequest(HttpMethod.Post, patientRoute) {
+            jsonBody(validRegistration)
+        }.run {
+            expectStatus(HttpStatusCode.OK)
+            val registration = receive<PatientRegistrationResponseDtoOut>()
+            assertEquals(slots.single(), registration.slot.id)
+            registration.patientId
+        }
+
+        // get patient by personal number
+        handleRequest(HttpMethod.Get, "${Routes.adminSectionPatient}?personalNumber=${validRegistration.personalNumber}") {
+            authorize()
+        }.run {
+            expectStatus(HttpStatusCode.OK)
+            val patient = receive<PatientDtoOut>()
+            assertEquals(patientId, patient.id)
+            assertEquals(validRegistration.personalNumber, patient.personalNumber)
+            assertEquals(validRegistration.email, patient.email)
+        }
+
+        // get patient by ID
+        handleRequest(HttpMethod.Get, "${Routes.adminSectionPatient}/$patientId") {
+            authorize()
+        }.run {
+            expectStatus(HttpStatusCode.OK)
+            val patient = receive<PatientDtoOut>()
+            assertEquals(patientId, patient.id)
+            assertEquals(validRegistration.personalNumber, patient.personalNumber)
+            assertEquals(validRegistration.email, patient.email)
+        }
+
+        // try to get patient id by the email
+        handleRequest(HttpMethod.Get, "${Routes.adminSectionPatient}/filter?email=${validRegistration.email}") {
+            authorize()
+        }.run {
+            expectStatus(HttpStatusCode.OK)
+            val patients = receive<List<PatientDtoOut>>()
+            assertEquals(1, patients.size)
+            assertEquals(patientId, patients.single().id)
+        }
+
+        // update email address
+        val newEmailAddress = "${UUID.randomUUID()}@mild.blue"
+        handleRequest(HttpMethod.Put, "${Routes.adminSectionPatient}/$patientId") {
+            authorize()
+            jsonBody(PatientUpdateDtoIn(email = newEmailAddress))
+        }.run {
+            expectStatus(HttpStatusCode.OK)
+            val patient = receive<PatientDtoOut>()
+            assertEquals(patientId, patient.id)
+            assertEquals(validRegistration.personalNumber, patient.personalNumber)
+            assertEquals(newEmailAddress, patient.email)
+        }
+
+        // delete patient
+        handleRequest(HttpMethod.Delete, "${Routes.adminSectionPatient}/$patientId") {
+            authorize()
+        }.run {
+            expectStatus(HttpStatusCode.OK)
+        }
+
+        // no patient should show up on filter search
+        handleRequest(HttpMethod.Get, "${Routes.adminSectionPatient}/filter") {
+            authorize()
+        }.run {
+            expectStatus(HttpStatusCode.OK)
+            val patients = receive<List<PatientDtoOut>>()
+            assertTrue { patients.isEmpty() }
+        }
+
+        // TODO #262 implement this - check that the patient was indeed deleted
+        // call endpoint with patient ID and check that the response is 404 - Not Found
+        // hint - see and try to understand what we're doing in the previous lines
+    }
+
 
     @Test
     fun `test concurrent booking`() = withTestApplication {
@@ -152,7 +247,6 @@ class PatientRoutesTest : ServerTestBase() {
             expectStatus(HttpStatusCode.NotAcceptable)
         }
     }
-
 
     @Test
     fun `patient should be registered`() = withTestApplication {
@@ -278,6 +372,14 @@ class PatientRoutesTest : ServerTestBase() {
         val incompleteAnswers = validRegistration.copy(answers = validRegistration.answers.dropLast(1))
         handleRequest(HttpMethod.Post, patientRoute) {
             jsonBody(incompleteAnswers)
+        }.run {
+            expectStatus(HttpStatusCode.BadRequest)
+        }
+
+        val invalidAnswerId =
+            validRegistration.copy(answers = validRegistration.answers.dropLast(1) + AnswerDtoIn(UUID.randomUUID(), true))
+        handleRequest(HttpMethod.Post, patientRoute) {
+            jsonBody(invalidAnswerId)
         }.run {
             expectStatus(HttpStatusCode.BadRequest)
         }
