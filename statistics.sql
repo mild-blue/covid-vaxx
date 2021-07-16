9ee25114d831e53f4605aec70721c1caf70fa91d
Tomáš Kouřim <kourim@outlook.com>
2021-07-16 10:21:50 +0200

    useful selects for second shot

--- statistics.sql
+++ statistics.sql
@@ -39,3 +39,39 @@ select *
 from patients
 where data_correctness_id is not null and vaccination_id is null;
 
+
+-- select / delete vaccination slots for second shot -> useful when someone cancels the appointment
+select *
+from vaccination_slots
+where "from" > '2021-07-16 00:31:51.463682' and
+    patient_id in (select id
+                   from patients
+                   where personal_number in ('9112554132', '0056280026', '9762041718', '0401085773', '9309040158',
+                                             '9304140021',
+                                             '9851166281', '9610160670', '0551274933', '0452255573', '9353241128',
+                                             '9261035410',
+                                             '0107230277', '9803073357', '9604140964', '8712081158', '9253251953',
+                                             '9207124091') or
+                       insurance_number in ('9112554132', '0056280026', '9762041718', '0401085773', '9309040158',
+                                            '9304140021',
+                                            '9851166281', '9610160670', '0551274933', '0452255573', '9353241128',
+                                            '9261035410',
+                                            '0107230277', '9803073357', '9604140964', '8712081158', '9253251953',
+                                            '9207124091'));
+
+-- select people who did not show up for their appointment (first shot)
+select first_name, last_name, personal_number, insurance_number, "from", phone_number, email
+from vaccination_slots
+         join patients p on p.id = vaccination_slots.patient_id
+where "from" < NOW() and vaccination_id is null;
+
+-- select people who did not show up for their appointment (second shot)
+select first_name, last_name, personal_number, insurance_number, "from", phone_number, email
+from vaccination_slots
+         join patients p on p.id = vaccination_slots.patient_id
+where "from" < NOW() and "from" > '2021-07-01 00:31:51.463682' and p.vaccination_second_dose_id is null;
+
+-- how many people should still come
+select count(*)
+from vaccination_slots
+where "from" >= now()
