cd907f6c5d3a2ba6da366f6e8132de40cf30c68e
Jan Kubant <kubant.jan@gmail.com>
2021-07-20 11:09:20 +0200

    Test it improved.

--- backend/src/test/kotlin/blue/mild/covid/vaxx/routes/ServiceRoutesTest.kt
+++ backend/src/test/kotlin/blue/mild/covid/vaxx/routes/ServiceRoutesTest.kt
@@ -1,26 +1,30 @@
 package blue.mild.covid.vaxx.routes
 
 import blue.mild.covid.vaxx.dao.model.InsuranceCompany
-import blue.mild.covid.vaxx.dao.model.Questions
+import blue.mild.covid.vaxx.dao.model.UserRole
+import blue.mild.covid.vaxx.dao.model.VaccinationBodyPart
 import blue.mild.covid.vaxx.dao.repository.PatientRepository
+import blue.mild.covid.vaxx.dao.repository.VaccinationRepository
 import blue.mild.covid.vaxx.dto.request.IsinJobDtoIn
+import blue.mild.covid.vaxx.dto.request.UserRegistrationDtoIn
 import blue.mild.covid.vaxx.dto.response.ApplicationInformationDtoOut
 import blue.mild.covid.vaxx.dto.response.IsinJobDtoOut
-import blue.mild.covid.vaxx.dto.response.PatientRegistrationResponseDtoOut
 import blue.mild.covid.vaxx.dto.response.SystemStatisticsDtoOut
-import blue.mild.covid.vaxx.service.QuestionService
+import blue.mild.covid.vaxx.service.PasswordHashProvider
+import blue.mild.covid.vaxx.service.UserService
 import blue.mild.covid.vaxx.utils.ServerTestBase
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.handleRequest
 import kotlinx.coroutines.runBlocking
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.Test
 import org.kodein.di.DI
 import org.kodein.di.bind
 import org.kodein.di.instance
 import org.kodein.di.singleton
+import java.time.Instant
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.test.assertEquals
 
 
@@ -59,11 +63,13 @@ class ServiceRoutesTest : ServerTestBase() {
     }
 
     @Test
+    @Suppress("LongMethod")
     fun `it should rerun isin job correctly even if patient has no answers`() = withTestApplication {
         val patientRepository by closestDI().instance<PatientRepository>()
+        val vaccinationRepository by closestDI().instance<VaccinationRepository>()
+        val userService by closestDI().instance<UserService>()
 
-        runBlocking {
-
+        val patientId = runBlocking {
             patientRepository.savePatient(
                 "alice",
                 "alice",
@@ -81,12 +87,46 @@ class ServiceRoutesTest : ServerTestBase() {
             )
         }
 
+        val userId = runBlocking {
+            userService.registerUser(
+                UserRegistrationDtoIn(
+                    firstName = "Test",
+                    lastName = "test",
+                    email = "Test",
+                    password = "test",
+                    role = UserRole.ADMIN
+                )
+            )
+        }
+
+        runBlocking {
+            vaccinationRepository.addVaccination(
+                patientId,
+                bodyPart = VaccinationBodyPart.BUTTOCK,
+                vaccinatedOn = Instant.now(),
+                vaccineSerialNumber = "test",
+                vaccineExpiration = LocalDate.parse(
+                    "2200-01-01",
+                    DateTimeFormatter.ISO_LOCAL_DATE
+                ),
+                userPerformingVaccination = userId.id,
+                doseNumber = 2
+            )
+
+        }
+
         val expected = IsinJobDtoOut(
-            0, 0, 0, 0,
+            0, 1, 0, 0,
             0, 0, 0,
             0, 0, 0
         )
-        val runWhat = IsinJobDtoIn()
+        val runWhat = IsinJobDtoIn(
+            exportPatientsInfo = true,
+            checkVaccinations = true,
+            exportVaccinationsFirstDose = true,
+            exportVaccinationsSecondDose = true,
+            validatePatients = true
+        )
 
 
         handleRequest(HttpMethod.Post, Routes.runIsinJob) {
